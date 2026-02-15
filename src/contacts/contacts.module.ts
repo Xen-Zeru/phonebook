66586081e13bfa573bce306e4f313524905f4ddc
Xen-Zeru <janlesteribanez@gmail.com>
2026-02-15 17:33:04 +0800

    dfsdf

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -4,12 +4,12 @@ import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
 import { Contact } from './entities/contact.entity';
 import { User } from '../users/entities/user.entity';
-import { UpdateContactDto } from './dto/update-contact.dto';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Contact, User])],
   controllers: [ContactsController],
   providers: [ContactsService],
-  exports: [ContactsService, UpdateContactDto]
+  exports: [ContactsService]
 })
 export class ContactsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
-import { UpdateUserDto } from './dto/update-user.dto';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService, UpdateUserDto],
+  exports: [UsersService],
 })
 export class UsersModule {}
