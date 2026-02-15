b116656f098a4ab44bc46755ed9d3aaa7be0c4c1
Xen-Zeru <janlesteribanez@gmail.com>
2026-02-15 20:31:23 +0800

    jjf-pb final

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   );
 
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
 
   const port = process.env.PORT || 3000;
